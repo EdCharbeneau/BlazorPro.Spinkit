838610ceb114704696199064243ff04fb05c2305
Ed Charbeneau <ed.charbeneau@gmail.com>
2019-12-19 14:23:50 -0500

    Add project files.

---
+++ BlazorApp21/Components/Spinkit/SpinnerType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp21.Components.Spinkit
+{
+    public enum SpinnerType
+    {
+        Bounce,
+        Chase,
+        Circle,
+        CircleFade,
+        Flow,
+        Grid,
+        Plane,
+        Pulse,
+        Swing,
+        Wander,
+        Wave
+    }
+}
