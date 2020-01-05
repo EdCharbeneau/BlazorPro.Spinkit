9236d277329122065d9adbca14742f602d403315
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-01-05 01:39:35 -0500

    Refactored spnners and added unit tests. Updated exampels.

---
+++ src/BlazorPro.Spinkit/Spinners/SpinnerBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorPro.Spinkit
+{
+    public class SpinnerBase : ComponentBase
+    {
+        [Parameter] public string Color { get; set; }
+        [Parameter] public bool Center { get; set; }
+        [Parameter] public string Size { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, Object>();
+
+    }
+}
