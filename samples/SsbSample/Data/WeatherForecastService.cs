3f919abf197caf8ba0b365e0dedeb282120bbb2e
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-01-10 15:15:32 -0500

    Improved Csb Samples Added Ssb Sample

---
+++ samples/SsbSample/Data/WeatherForecastService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SsbSample.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            await Task.Delay(3000);
+            var rng = new Random();
+            return await Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
